50697b2bba22814a48590256ed37794519d6342e
VanshKela <vanshkela2@gmail.com>
2021-11-17 21:53:32 +0530

    Graph edit

--- lib/utils/graphs.dart
+++ lib/utils/graphs.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:sizer/sizer.dart';
 
 class _ChartData {
   _ChartData(this.x, this.y);
@@ -14,7 +15,7 @@ Widget StatePieChart(List orderData) {
   List<_ChartData> chartData = [];
   Map<String, int> orderMap = {};
   orderData.forEach((order) {
-    totalSale+=order['total'];
+    totalSale += order['total'];
     if (orderMap.containsKey(order['state']))
       orderMap[order['state']] =
           (orderMap[order['state']]! + order['total']) as int;
@@ -25,18 +26,19 @@ Widget StatePieChart(List orderData) {
     chartData.add(_ChartData(key, value));
   });
   return Container(
+      width: 200.w,
       child: SfCircularChart(series: <CircularSeries>[
-    // Render pie chart
-    PieSeries<_ChartData, String>(
-      dataSource: chartData,
-      xValueMapper: (_ChartData data, _) => data.x,
-      yValueMapper: (_ChartData data, _) => data.y,
-      dataLabelMapper: (_ChartData data, _) {
-        return "${data.x}: ${(data.y/totalSale*100).toStringAsFixed(2)}%";
-      },
-      dataLabelSettings: DataLabelSettings(isVisible: true),
-    )
-  ]));
+        // Render pie chart
+        PieSeries<_ChartData, String>(
+          dataSource: chartData,
+          xValueMapper: (_ChartData data, _) => data.x,
+          yValueMapper: (_ChartData data, _) => data.y,
+          dataLabelMapper: (_ChartData data, _) {
+            return "${data.x}: ${(data.y / totalSale * 100).toStringAsFixed(2)}%";
+          },
+          dataLabelSettings: DataLabelSettings(isVisible: true),
+        )
+      ]));
 }
 
 Widget EmployeeBarGraph(List orderData) {
@@ -53,20 +55,26 @@ Widget EmployeeBarGraph(List orderData) {
     data.add(_ChartData(key, value));
   });
   TooltipBehavior _tooltip = TooltipBehavior(enable: true);
-  return Container(
-      child: SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          primaryYAxis: NumericAxis(
-              minimum: 0,
-              maximum: orderMap.values.reduce(max).toDouble(),
-              interval: 1000),
-          tooltipBehavior: _tooltip,
-          series: <ChartSeries<_ChartData, String>>[
-        ColumnSeries<_ChartData, String>(
-            dataSource: data,
-            xValueMapper: (_ChartData data, _) => data.x,
-            yValueMapper: (_ChartData data, _) => data.y,
-            name: 'Gold',
-            color: Color.fromRGBO(8, 142, 255, 1))
-      ]));
+  return Scrollbar(
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+          width: data.length < 10 ? 100.w : 30.0 * data.length,
+          child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              primaryYAxis: NumericAxis(
+                  minimum: 0,
+                  maximum: orderMap.values.reduce(max).toDouble(),
+                  interval: 1000),
+              tooltipBehavior: _tooltip,
+              series: <ChartSeries<_ChartData, String>>[
+                ColumnSeries<_ChartData, String>(
+                    dataSource: data,
+                    xValueMapper: (_ChartData data, _) => data.x,
+                    yValueMapper: (_ChartData data, _) => data.y,
+                    name: 'Gold',
+                    color: Color.fromRGBO(8, 142, 255, 1))
+              ])),
+    ),
+  );
 }
