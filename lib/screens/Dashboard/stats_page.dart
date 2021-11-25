0524f1a594c14dc7561c6850eac7b621be38c127
VanshKela <vanshkela2@gmail.com>
2021-11-25 14:21:07 +0530

    Added excel

--- lib/screens/Dashboard/stats_page.dart
+++ lib/screens/Dashboard/stats_page.dart
@@ -1,7 +1,11 @@
 import 'package:chef_gram_admin/database_service.dart';
 import 'package:chef_gram_admin/utils/graphs.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' as xlsio;
+import 'dart:io';
+import 'package:open_file/open_file.dart';
 
 class StatsPage extends StatefulWidget {
   const StatsPage({Key? key, required this.orderData}) : super(key: key);
@@ -28,6 +32,20 @@ class _StatsPageState extends State<StatsPage> {
     return graphs;
   }
 
+  void getExcel() async{
+    final xlsio.Workbook workbook = new xlsio.Workbook();
+    workbook.worksheets[0];
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    final String path = (await getApplicationSupportDirectory()).path;
+    final String fileName = '$path/Output.xlsx';
+    final File file = File(fileName);
+    await file.writeAsBytes(bytes, flush: true).then((value) {
+      OpenFile.open(fileName);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     DateTime startDate =
@@ -36,6 +54,7 @@ class _StatsPageState extends State<StatsPage> {
         Provider.of<DatabaseService>(context, listen: false).filters.endDate;
     return Scaffold(
       appBar: AppBar(
+        actions: [IconButton(onPressed: getExcel, icon: Icon(Icons.download))],
         centerTitle: true,
         title: Text(
             "Analysis ${"${startDate.day}-${startDate.month}-${startDate.year}"} to ${"${endDate.day}-${endDate.month}-${endDate.year}"}"),
