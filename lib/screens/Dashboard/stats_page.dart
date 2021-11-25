b3ebfa9ee05b5bd1a1ea89d8e23b258c41052dbf
VanshKela <vanshkela2@gmail.com>
2021-11-25 19:39:06 +0530

    EVERYTHING WORKS

--- lib/screens/Dashboard/stats_page.dart
+++ lib/screens/Dashboard/stats_page.dart
@@ -35,13 +35,44 @@ class _StatsPageState extends State<StatsPage> {
   void getExcel() async{
     final xlsio.Workbook workbook = new xlsio.Workbook();
     xlsio.Worksheet sheet = workbook.worksheets[0];
-    int row = 1;
+    sheet.getRangeByIndex(1, 1).setText('Shop Name');
+    sheet.getRangeByIndex(1, 2).setText('Customer Name');
+    sheet.getRangeByIndex(1, 3).setText('State');
+    sheet.getRangeByIndex(1, 4).setText('City');
+    sheet.getRangeByIndex(1, 5).setText('Beat');
+    sheet.getRangeByIndex(1, 6).setText('Address');
+    sheet.getRangeByIndex(1, 7).setText('Order Taken By');
+    sheet.getRangeByIndex(1, 8).setText('Date Time');
+    sheet.getRangeByIndex(1, 9).setText('Item');
+    sheet.getRangeByIndex(1, 10).setText('Quantity (gm)');
+    sheet.getRangeByIndex(1, 11).setText('Boxes');
+    sheet.getRangeByIndex(1, 12).setText('Price');
+    sheet.getRangeByIndex(1, 13).setText('Total');
+    int row = 2;
     widget.orderData.forEach((order) {
       String shopName = order.get('shopName');
+      String customerName = order.get('customerName');
+      String state = order.get('state');
+      String city = order.get('city');
+      String beat = order.get('beat');
+      String address = order.get('address');
+      String orderTakenBy = order.get('orderTakenBy');
+      DateTime dateTime = order.get('dateTime').toDate();
       List items = order.get('items');
       for (int i=0; i<items.length;i++) {
         sheet.getRangeByIndex(row, 1).setText(shopName);
-        sheet.getRangeByIndex(row, 2).setText(items[i]['name']);
+        sheet.getRangeByIndex(row, 2).setText(customerName);
+        sheet.getRangeByIndex(row, 3).setText(state);
+        sheet.getRangeByIndex(row, 4).setText(city);
+        sheet.getRangeByIndex(row, 5).setText(beat);
+        sheet.getRangeByIndex(row, 6).setText(address);
+        sheet.getRangeByIndex(row, 7).setText(orderTakenBy);
+        sheet.getRangeByIndex(row, 8).setDateTime(dateTime);
+        sheet.getRangeByIndex(row, 9).setText(items[i]['name']);
+        sheet.getRangeByIndex(row, 10).setText((items[i]['quantity']).toString());
+        sheet.getRangeByIndex(row, 11).setText((items[i]['boxes']).toString());
+        sheet.getRangeByIndex(row, 12).setText((items[i]['price']).toString());
+        sheet.getRangeByIndex(row, 13).setText((items[i]['boxes'] * items[i]['price']).toString());
         row++;
       }
     });
